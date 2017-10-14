f31cf7b4a427d86bddb47e3b43ccd7c16881f84a
thinkpad <547093794@qq.com>
2017-10-14 23:41:58 +0800

    1.首次提交 2.第二次提交

--- app/src/main/java/com/edu/niit/mediaplay/MainActivity.java
+++ app/src/main/java/com/edu/niit/mediaplay/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.io.IOException;
-//测试  本行无
+//测试  本行
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     MediaPlayer mediaPlayer;
     Button btn_paly, btn_pause, btn_stop;
